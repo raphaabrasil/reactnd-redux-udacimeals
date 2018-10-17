8588b475fe28cafe0a1a034d311cb04bed6ea8fc
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-10-16 22:00:32 -0300

    Enables redux dev tools

--- src/index.js
+++ src/index.js
@@ -6,9 +6,10 @@ import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 import reducer from './reducers'
 
-const store = createStore(reducer)
-
-console.log(store.getState())
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

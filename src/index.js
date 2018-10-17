c26dc7848994706d5eb056285ea77d805f62467a
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-10-16 21:48:30 -0300

    Creates store

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+import reducer from './reducers'
+
+const store = createStore(reducer)
+
+console.log(store.getState())
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

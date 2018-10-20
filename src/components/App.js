c38d8481b3814c6ffcc6e99f8d069b2039ce0108
Raphael Brasil <raphael.brasil@resultadosdigitais.com.br>
2018-10-20 14:57:20 -0300

    Connects app to the store and pass it state for the component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
 class App extends Component {
   render() {
+    console.log(this.props)
     return (
       <div>
         Hello World
@@ -10,4 +12,17 @@ class App extends Component {
   }
 }
 
-export default App
+const mapStateToProps = (calendar) => {
+  const dayOrders = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+  return {
+    calendar: dayOrders.map( ( day ) => ({
+      day,
+      meals: Object.keys( calendar[day] ).reduce(( meals, meal ) => {
+        meals[meal] = calendar[day][meal] ? calendar[day][meal] : null
+        return meals
+      }, {})
+    }))
+  }
+}
+export default connect(mapStateToProps)(App)
